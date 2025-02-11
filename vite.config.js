026007059c070e2665c1b79c3339b3d300040949
Bruno Dias <bdd.vasconcelos@icloud.com>
2025-02-11 10:25:53 -0400

    chore (deps & config): implement configs and add deps on project

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,33 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/",
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/ts/app.tsx"],
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        host: "0.0.0.0",
-        hmr: {
-            host: "localhost",
-        },
-        watch: {
-            usePolling: true,
-        },
+  base: "/",
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/ts/app.tsx"],
+      refresh: true,
+    }),
+    react(),
+  ],
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      host: "localhost",
     },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "resources/ts"),
-            "@css": path.resolve(__dirname, "resources/css"),
-        },
+    watch: {
+      usePolling: true,
     },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "resources/ts"),
+      "@components": path.resolve(__dirname, "resources/ts/components"),
+      "@pages": path.resolve(__dirname, "resources/ts/pages"),
+      "@utils": path.resolve(__dirname, "resources/ts/utils"),
+      "@hooks": path.resolve(__dirname, "resources/ts/hooks"),
+      "@css": path.resolve(__dirname, "resources/css"),
+      "@assets": path.resolve(__dirname, "resources/ts/assets"),
+      "@schemas": path.resolve(__dirname, "resources/ts/schemas"),
+    },
+  },
 });
