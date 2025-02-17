a33106968ea86c6275857eb7c920fb264e7382d8
Bruno Dias <bdd.vasconcelos@icloud.com>
2025-02-17 19:16:32 -0400

    feat: update key of UserFactory and format package.json

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,34 +11,34 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+  /**
+   * The current password being used by the factory.
+   */
+  protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => 'admin@email.com',
-            'email_verified_at' => now(),
-            'roles' => [ 'admin'],
-            'password' => static::$password ??= Hash::make('password'),
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'name' => fake()->name(),
+      'email' => 'admin@email.com',
+      'email_verified_at' => now(),
+      'roles' => ['admin'],
+      'password' => static::$password ??= Hash::make('password'),
+    ];
+  }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+  /**
+   * Indicate that the model's email address should be unverified.
+   */
+  public function unverified(): static
+  {
+    return $this->state(fn(array $attributes) => [
+      'email_verified_at' => null,
+    ]);
+  }
 }
