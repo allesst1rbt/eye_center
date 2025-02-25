513b04c0b8cd4266f012c224a5c07a4982beea7b
Bruno Dias <bdd.vasconcelos@icloud.com>
2025-02-24 20:21:20 -0400

    feat: create protected route on route archive

--- resources/ts/routes.tsx
+++ resources/ts/routes.tsx
@@ -1,9 +1,26 @@
+import { ReactElement } from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import SignIn from "./pages/auth/SignIn";
 import Home from "./pages/Home";
 
+const isAuthenticated = (): boolean => !!localStorage.getItem("token");
+
+type ProtectedRouteProps = {
+  element: ReactElement;
+};
+
+const ProtectedRoute = ({ element }: ProtectedRouteProps): ReactElement => {
+  return isAuthenticated() ? element : <Navigate to="/" />;
+};
+
 export const router = createBrowserRouter([
-  { path: "/", element: <SignIn /> },
-  { path: "/dashboard", element: <Home /> },
-  { path: "*", element: <Navigate to="/login" /> },
+  {
+    path: "/",
+    element: isAuthenticated() ? <Navigate to="/dashboard" /> : <SignIn />,
+  },
+  { path: "/dashboard", element: <ProtectedRoute element={<Home />} /> },
+  {
+    path: "*",
+    element: <Navigate to={isAuthenticated() ? "/dashboard" : "/"} />,
+  },
 ]);
