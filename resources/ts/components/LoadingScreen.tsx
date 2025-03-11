080fbba6290e01b8db7beb70f3368db9ace24cb8
Bruno Dias de Vasconcelos <bdd.vasconcelos@icloud.com>
2025-03-11 15:16:33 -0400

    chore: remove react-loader-spinner and add react-spinners

--- resources/ts/components/LoadingScreen.tsx
+++ resources/ts/components/LoadingScreen.tsx
@@ -1,18 +1,21 @@
-import "@css/LoadingScreen.css";
-import { TailSpin } from "react-loader-spinner";
+import { CSSProperties } from "react";
+import { ClipLoader } from "react-spinners";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+};
 
 export function LoadingScreen() {
   return (
     <div className="root">
-      <TailSpin
-        visible={true}
-        height="80"
-        width="80"
-        color="#ffffff"
-        ariaLabel="tail-spin-loading"
-        radius="1"
-        wrapperStyle={{}}
-        wrapperClass=""
+      <ClipLoader
+        color={"#FFFFFF"}
+        loading={true}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
       />
     </div>
   );
