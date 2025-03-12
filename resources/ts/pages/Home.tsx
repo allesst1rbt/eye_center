51df065cb0ec1c6123a09b81c9ac47cf68fc6dbe
Bruno Dias <bdd.vasconcelos@icloud.com>
2025-03-12 14:04:47 -0400

    feat: implement home table

--- resources/ts/pages/Home.tsx
+++ resources/ts/pages/Home.tsx
@@ -1,43 +1,87 @@
 import CustomButton from "@/components/CustomButton";
 import "@css/Home.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { IconButton, Paper } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useState } from "react";
 
-interface Client {
-  name: string;
-  email: string;
-  number: string;
+interface Order {
+  id: number;
+  customerName: string;
+  customerEmail: string;
+  customerNumber: string;
+  lensName: string;
+  lensFabricant: string;
+  lensFamily: string;
+  lensDurability: number;
 }
 
-interface Lens {
-  name: string;
-  fabricant: string;
-  family: string;
-  durability: number;
-}
-interface Order {
-  client: Client;
-  lens: Lens;
-}
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "customerName",
+    headerName: "Cliente",
+    width: 180,
+  },
+  {
+    field: "lensName",
+    headerName: "Lente",
+    width: 180,
+  },
+  { field: "lensDurability", headerName: "Prazo", width: 120 },
+  {
+    field: "actions",
+    headerName: "Ações",
+    width: 120,
+    sortable: false,
+    renderCell: (params) => (
+      <>
+        <IconButton color="primary" onClick={() => handleEdit(params.row.id)}>
+          <EditIcon />
+        </IconButton>
+        <IconButton color="error" onClick={() => handleDelete(params.row.id)}>
+          <DeleteIcon />
+        </IconButton>
+      </>
+    ),
+  },
+];
+
+const handleEdit = (id: number) => {
+  console.log("Editar pedido:", id);
+};
+
+const handleDelete = (id: number) => {
+  console.log("Excluir pedido:", id);
+};
 
 export default function Home() {
   const [orders, setOrders] = useState<Order[]>([
     {
-      client: {
-        email: "bdd.vasconcelos@gmail.com",
-        name: "Bruno Dias de Vasconcelos",
-        number: "(92) 98170-5996",
-      },
-      lens: {
-        name: "",
-        fabricant: "",
-        durability: 20,
-        family: "",
-      },
+      id: 1,
+      customerName: "Bruno Dias",
+      customerEmail: "bdd.vasconcelos@email.com",
+      customerNumber: "(92) 98170-5996",
+      lensName: "Lente XYZ",
+      lensFabricant: "Fabricante A",
+      lensDurability: 20,
+      lensFamily: "Família 1",
+    },
+    {
+      id: 2,
+      customerName: "Bruno Dias",
+      customerEmail: "bdd.vasconcelos@email.com",
+      customerNumber: "(92) 98170-5996",
+      lensName: "Lente XYZ",
+      lensFabricant: "Fabricante A",
+      lensDurability: 20,
+      lensFamily: "Família 1",
     },
   ]);
 
   const subtitle = `${orders.length} ${
-    orders.length == 1 ? "pedido encontrado" : "pedidos encontrados"
+    orders.length === 1 ? "pedido encontrado" : "pedidos encontrados"
   }`;
 
   return (
@@ -52,13 +96,22 @@ export default function Home() {
               color: "#fff",
               backgroundColor: "#00c3b5",
               borderRadius: "5px",
-              fontSize: "1rem",
+              fontSize: "1.1rem",
               padding: "0.5vh 2vh",
             }}
           />
         </div>
         <h2 className="home-subtitle">{subtitle}</h2>
       </div>
+
+      <Paper sx={{ height: "85%", width: "100%", marginTop: "5vh" }}>
+        <DataGrid
+          rows={orders}
+          columns={columns}
+          pageSizeOptions={[5, 10]}
+          sx={{ border: 0 }}
+        />
+      </Paper>
     </div>
   );
 }
