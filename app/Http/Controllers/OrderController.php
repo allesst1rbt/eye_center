4c9931ca3f77b35d6114aaab90c8c82bde7bb517
carlos dias <carloseduardodc090@gmail.com>
2025-04-26 00:07:49 -0400

    message

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -67,48 +67,88 @@ class OrderController extends Controller
         $result = $messageService->sendText(
             $order->customer_number,
             "Olá {$order->customer_name},
-                Agradecemos imensamente pela sua confiança em adaptar suas lentes de contato conosco.
-                Estamos muito felizes por você ter escolhido a Clínica Eye Center!
-                Preparamos algumas instruções simples para o seu uso
-                ATENÇÃO AOS CUIDADOS COM SUA LENTE:
-                1. Higiene das Mãos:
-                - Lave e seque bem as mãos antes de manusear as lentes.
-                2. Colocação das Lentes:
-                - Verifique se a lente está limpa e sem danos.
-                - Coloque a lente no dedo indicador, puxe as pálpebras e coloque-a sobre o olho.
-                - Pisque suavemente para ajustar a lente.
-                3. Remoção das Lentes:
-                - Lave as mãos antes de remover as lentes.
-                - Puxe a pálpebra inferior e use o polegar e o indicador para pinçar e retirar a lente.
-                4. Armazenamento:
-                - Para lentes GELATINOSAS (coloridas e incolores, mensais e anuais): use sempre solução
-                recomendada para armazenamento, indicamos o uso da solução Renu.
-                - Para lentes RÍGIDAS E ESCLERAIS: use sempre solução recomendada para armazenamento,
-                indicamos o uso diário da solução Boston Simplus.
-                - Troque a solução do estojo a cada remoção das lentes.
-                5. Tempo de Uso:
-                - Seguir as orientações do fabricante.
-                - Respeite o prazo de validade das suas lentes, para evitar infecções.
-                6. Cuidados Adicionais:
-                - Evite dormir com lentes (a menos que orientado e discutido com seu médico).
-                - Não use lentes danificadas ou mal higienizadas.
-                - Evite nadar com as lentes.
-                7. Sinais de Alerta:
-                - Remova as lentes imediatamente e consulte-nos se você sentir:
-                ▪ Dor, vermelhidão ou irritação nos olhos.
-                ▪ Visão embaçada ou halos ao redor das luzes.
-                ▪ Sensibilidade à luz.
-                ▪ Secreção ou lacrimejamento excessivo.
-                8. Consultas Regulares:
-                - Agende consultas oftalmológicas regulares para monitorar a saúde dos seus olhos e garantir
-                que suas lentes de contato continuem adequadas para você. A frequência das consultas será
-                definida pelo seu oftalmologista
-                Caso tenha qualquer dúvida ou precise de assistência, não hesite em nos contatar por aqui ou
-                por nosso instagram https://www.instagram.com/eyecentermanaus?igsh=ajkwZTVsaG0xZnJt
-                Desejamos que as lentes atendam suas expectativas e que você tenha uma excelente
-                experiência com ele.
-                Atenciosamente,
-                EYE CENTER.
+
+Agradecemos imensamente pela sua confiança em adaptar suas lentes de contato conosco.
+
+Estamos muito felizes por você ter escolhido a Clínica Eye Center!
+
+Preparamos algumas instruções simples para o seu uso
+
+ATENÇÃO AOS CUIDADOS COM SUA LENTE:
+
+1. Higiene das Mãos:
+
+- Lave e seque bem as mãos antes de manusear as lentes.
+
+2. Colocação das Lentes:
+
+- Verifique se a lente está limpa e sem danos.
+
+- Coloque a lente no dedo indicador, puxe as pálpebras e coloque-a sobre o olho.
+
+- Pisque suavemente para ajustar a lente.
+
+3. Remoção das Lentes:
+
+- Lave as mãos antes de remover as lentes.
+
+- Puxe a pálpebra inferior e use o polegar e o indicador para pinçar e retirar a lente.
+
+4. Armazenamento:
+
+- Para lentes GELATINOSAS (coloridas e incolores, mensais e anuais): use sempre solução
+
+recomendada para armazenamento, indicamos o uso da solução Renu.
+
+- Para lentes RÍGIDAS E ESCLERAIS: use sempre solução recomendada para armazenamento,
+
+indicamos o uso diário da solução Boston Simplus.
+
+- Troque a solução do estojo a cada remoção das lentes.
+
+5. Tempo de Uso:
+
+- Seguir as orientações do fabricante.
+
+- Respeite o prazo de validade das suas lentes, para evitar infecções.
+
+6. Cuidados Adicionais:
+
+- Evite dormir com lentes (a menos que orientado e discutido com seu médico).
+
+- Não use lentes danificadas ou mal higienizadas.
+
+- Evite nadar com as lentes.7. Sinais de Alerta:
+
+- Remova as lentes imediatamente e consulte-nos se você sentir:
+
+▪ Dor, vermelhidão ou irritação nos olhos.
+
+▪ Visão embaçada ou halos ao redor das luzes.
+
+▪ Sensibilidade à luz.
+
+▪ Secreção ou lacrimejamento excessivo.
+
+8. Consultas Regulares:
+
+- Agende consultas oftalmológicas regulares para monitorar a saúde dos seus olhos e garantir
+
+que suas lentes de contato continuem adequadas para você. A frequência das consultas será
+
+definida pelo seu oftalmologista
+
+Caso tenha qualquer dúvida ou precise de assistência, não hesite em nos contatar por aqui ou
+
+por nosso instagram https://www.instagram.com/eyecentermanaus?igsh=ajkwZTVsaG0xZnJt
+
+Desejamos que as lentes atendam suas expectativas e que você tenha uma excelente
+
+experiência com ele.
+
+Atenciosamente,
+
+EYE CENTER.
                 "
         );
     }
