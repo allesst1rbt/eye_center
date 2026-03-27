72abdc645df052e985b7d828cc22ed995e3597c3
all3sstirbt <carloseduardodc090@gmail.com>
2026-03-27 15:55:58 -0400

    chore: adjust tests

--- app/Models/Terms.php
+++ app/Models/Terms.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Terms extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
         'expire_date',
