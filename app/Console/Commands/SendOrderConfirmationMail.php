f3b1887f823bdd96616a34294e31ea129871bc34
all3sstirbt <carloseduardodc090@gmail.com>
2026-04-02 00:07:31 -0400

    chore: send order confirmation job

---
+++ app/Console/Commands/SendOrderConfirmationMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use App\Services\OrderNotificationService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class SendOrderConfirmationMail extends Command
+{
+    protected $signature = 'orders:send-confirmation';
+    protected $description = 'Send order confirmation to purchases that have not been sent yet';
+
+    public function __construct(private OrderNotificationService $notificationService)
+    {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        Log::info('orders:send-confirmation started');
+
+        $orders = Order::where('order_confirmation', false)->get();
+        $notified = 0;
+
+        foreach ($orders as $order) {
+            $this->notificationService->notifyOrderCreated($order);
+            $this->info("Confirmation queued for {$order->customer_name}");
+            Log::info("Confirmation queued for {$order->customer_name}");
+            $notified++;
+        }
+
+        $this->info("Done. {$notified} confirmation(s) queued.");
+        Log::info('orders:send-confirmation finished', ['notified' => $notified]);
+    }
+}
